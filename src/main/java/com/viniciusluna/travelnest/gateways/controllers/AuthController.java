3b5efc5bf6e3c30f5264b8558a02b4f825bfa801
Vinicius Luna <viniciusluna@yahoo.com>
2024-11-19 23:14:40 -0300

    refact(auth): remove logic from controller and create an auth service

--- src/main/java/com/viniciusluna/travelnest/gateways/controllers/AuthController.java
+++ src/main/java/com/viniciusluna/travelnest/gateways/controllers/AuthController.java
@@ -1,14 +1,10 @@
 package com.viniciusluna.travelnest.gateways.controllers;
 
-import com.viniciusluna.travelnest.domain.User;
-import com.viniciusluna.travelnest.gateways.repositories.UserRepository;
 import com.viniciusluna.travelnest.gateways.requests.AuthRequest;
-import com.viniciusluna.travelnest.gateways.responses.AuthResponse;
+import com.viniciusluna.travelnest.usecases.impl.AuthServiceImpl;
 import com.viniciusluna.travelnest.usecases.impl.TokenServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserRepository repository;
-    @Autowired
-    private TokenServiceImpl tokenService;
+    private AuthServiceImpl authService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody AuthRequest data){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-
-        return ResponseEntity.ok(new AuthResponse(token));
+    public ResponseEntity login(@RequestBody AuthRequest body){
+        return authService.login(body.getEmail(), body.getPassword());
     }
 }
