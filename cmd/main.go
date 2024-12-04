4886ff1722bc4c6f6d3db9d7bac4d65096b4345c
Vinicius Luna <viniciusluna@yahoo.com>
2024-12-04 00:41:49 -0300

    feat: routes register

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"travelnest/internal/controller"
+	"travelnest/internal/database"
+	"travelnest/internal/repository"
+	usecase "travelnest/internal/use-case"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 		server := gin.Default()
+
+		dbConnection, err := database.ConnectDB()
+		if(err != nil) {
+			panic(err)
+		}
+		
+		RoomRepository := repository.NewRoomRepository(dbConnection)
+		RoomUseCase := usecase.NewRoomUseCase(RoomRepository)
+		RoomController := controller.NewRoomController(RoomUseCase)
 		
 		server.GET("/ping", func(c *gin.Context) {
 			c.JSON(200, gin.H{
 				"message": "pong",
 			})
 		})
+		
+		server.GET("/rooms", RoomController.GetRooms)
+
 		
 		server.Run(":8080")
 }
